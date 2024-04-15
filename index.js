5d266b0f9bd10e97f0c6157635d588a2e7a80bcb
Jacek <jpajak.code@gmail.com>
2024-04-15 19:15:24 +0100

    Player movement (no animations)

--- index.js
+++ index.js
@@ -13,8 +13,43 @@ mapImage.src = './images/XmonMap-Area1.png';
 const playerImage = new Image();
 playerImage.src = './images/playerDown.png';
 
-mapImage.onload = () => {
-  ctx.drawImage(mapImage, -64, -410);
+class Sprite {
+  constructor({ position, velocity, mapImage }) {
+    this.position = position;
+    this.mapImage = mapImage;
+  }
+
+  draw() {
+    ctx.drawImage(this.mapImage, this.position.x, this.position.y);
+  }
+}
+
+const background = new Sprite({
+  position: {
+    x: -64,
+    y: -410,
+  },
+  mapImage: mapImage,
+});
+
+const keys = {
+  w: {
+    pressed: false,
+  },
+  a: {
+    pressed: false,
+  },
+  s: {
+    pressed: false,
+  },
+  d: {
+    pressed: false,
+  },
+};
+
+function animate() {
+  window.requestAnimationFrame(animate);
+  background.draw();
   ctx.drawImage(
     playerImage,
     0, // Cropping starting point on X axis
@@ -26,4 +61,49 @@ mapImage.onload = () => {
     playerImage.width / 4,
     playerImage.height
   );
-};
+
+  if (keys.w.pressed && lastKey === 'w') background.position.y += 3;
+  else if (keys.a.pressed && lastKey === 'a') background.position.x += 3;
+  else if (keys.s.pressed && lastKey === 's') background.position.y -= 3;
+  else if (keys.d.pressed && lastKey === 'd') background.position.x -= 3;
+}
+animate();
+
+let lastKey = '';
+window.addEventListener('keydown', (e) => {
+  switch (e.key) {
+    case 'w':
+      keys.w.pressed = true;
+      lastKey = 'w';
+      break;
+    case 'a':
+      keys.a.pressed = true;
+      lastKey = 'a';
+      break;
+    case 's':
+      keys.s.pressed = true;
+      lastKey = 's';
+      break;
+    case 'd':
+      keys.d.pressed = true;
+      lastKey = 'd';
+      break;
+  }
+});
+
+window.addEventListener('keyup', (e) => {
+  switch (e.key) {
+    case 'w':
+      keys.w.pressed = false;
+      break;
+    case 'a':
+      keys.a.pressed = false;
+      break;
+    case 's':
+      keys.s.pressed = false;
+      break;
+    case 'd':
+      keys.d.pressed = false;
+      break;
+  }
+});
